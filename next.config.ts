4bdb47c2cab1dd627b643af0563e46eb25439092
Henry Cai <huacong.cai@gmail.com>
2025-12-05 15:20:15 -0800

    fix(pino): add thread-stream to serverExternalPackages list and depdency list

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
-  serverExternalPackages: ["pino", "pino-pretty", "pino-roll"],
+  serverExternalPackages: ["pino", "thread-stream", "pino-pretty", "pino-roll"],
 };
 
 export default nextConfig;
