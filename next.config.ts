3b1e26232174432f28d0c43d7a9b91cc75d3fbeb
Henry Cai <huacong.cai@gmail.com>
2025-12-08 17:58:55 -0800

    build(deps): update next to v16.1.0-canary.16

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
-  serverExternalPackages: ["pino", "thread-stream", "pino-pretty", "pino-roll"],
 };
 
 export default nextConfig;
